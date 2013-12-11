1bf04ac65de07de6ca667d3f539fd0a2b3940f62
juliehui <j.s.hui13@gmail.com>
2013-12-10 23:01:29 -0600

    trying out local storage

---
+++ Jeopardy_localstorage.js
@@ -0,0 +1,68 @@
+//var test = 1
+var jeopardy = (function() {        //what is this again?
+    var questions = [{'text': ' This restaurant is the best in Evanston', 'answer': 'What is the Chicken Shack', 'pointValue' : 100},
+    
+    {'text': ' This the best school in the universe', 'answer': 'What is Northwestern', 'pointValue' : 200}                 
+                    ]
+    localStorage.setItem('questions', JSON.stringify(questions))
+
+    var exports = {};
+    var score = 0;
+    var currentQuestion = 0;
+    
+    function checkAnswer(){
+        // 1. check if something is the correct answer
+        if($('.answerBox').val()==localStorage.getItem(questions[currentQuestion]['answer'])){
+            alert("Correct!")
+            // 2. if it is, increment the score and call displayScore
+            score+=localStorage.getItem(questions[currentQuestion]['pointValue'])
+            displayScore(); 
+            currentQuestion++
+            displayQuestion()
+        }else{
+            alert("Incorrect!")
+        }
+        $('.answerBox').val('');  // 3. regardless, move to the next question  
+    }
+
+    function displayQuestion(){
+        // displays the current question
+        $('.question').html('Question:' + localStorage.getItem(questions[currentQuestion]['text']));
+          
+    }
+
+    function displayScore(){
+        // displays the score
+        $('.score').html('Your score:' + score);
+    }
+
+    
+    function setup(){
+        // show the first question
+        displayQuestion();
+        displayScore();
+        // attach checkAnswer to the button
+        $('#answerButton').click(function(){
+            checkAnswer();
+        });
+        
+    }
+
+    function checkHTML(){       //not sure if this works
+        function supports_html5_storage() {
+        try {
+            return 'localStorage' in window && window['localStorage'] !== null;
+        } catch (e) {
+        return false;
+        }
+        }
+    }
+
+$(document).ready(function(){
+    //jeopardy.checkHTML();
+    jeopardy.setup();
+});
+
+    exports.setup = setup;          //what is this?
+    return exports;
+})()
